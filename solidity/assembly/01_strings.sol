485ee455d57854d1d32fe168a8126bcbc1ca1398
BruceCoins <bruce369yyds@gmail.com>
2023-12-24 08:56:27 +0800

    modifier

--- solidity/assembly/01_strings.sol
+++ solidity/assembly/01_strings.sol
@@ -3,18 +3,21 @@ pragma solidity ^0.8.0;
 
 library strings{
     struct slice{
-        uint _len;
-        uint _ptr;
+        uint _len; //长度
+        uint _ptr; //指针
     }
 
     function memcpy(uint dest, uint src, uint length) private pure{
 
         for (; length >= 32; length -= 32){
+            //引入内联汇编
             assembly{
+                // mload()从内存中读取，
+                // mstore()用读取出的数据替换dest
                 mstore(dest,mload(src))
             }
-            dest += 32;
-            src += 32;
+            dest += 32;  // 加偏移量
+            src += 32;   
         }
     }
 }
