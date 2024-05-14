bc80d99215af8aeeb422cda18d1fa30604e4e193
bruce <bruce369@yyds@gmail.com>
2024-05-14 22:13:25 +0800

    del

--- 0x0005 project/Proxy-Box/hardhat.config.js
+++
@@ -1,9 +0,0 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-
-require("@nomicfoundation/hardhat-toolbox");
-require('@openzeppelin/hardhat-upgrades');
-require('dotenv').comfig;
-
-module.exports = {
-  solidity: "0.8.24",
-};
